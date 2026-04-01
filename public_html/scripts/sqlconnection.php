5a82a3ba2bba9da6a7ac4d3395a99781c8e5d762
Anirudh Dutt <1468519+aneroid@users.noreply.github.com>
2026-04-01 16:47:02 +0530

    use a PHP file for config instead of Env

--- public_html/scripts/sqlconnection.php
+++ public_html/scripts/sqlconnection.php
@@ -1,10 +1,7 @@
 <?php
-$con = new mysqli(
-    getenv("DB_HOST", true) ?: die("'DB_HOST' is not set"),
-    getenv("DB_USER", true) ?: die("'DB_USER' is not set"),
-    getenv("DB_PASS", true) ?: die("'DB_PASS' is not set"),
-    getenv("DB_NAME", true) ?: die("'DB_NAME' is not set"),
-);
+require __DIR__ . "/../config.php";
+
+$con = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
