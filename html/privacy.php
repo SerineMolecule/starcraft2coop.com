ab3c9d126510e713c817ab69f945214193624e5e
Serine <serinemolecule@gmail.com>
2026-04-20 20:57:14 -0700

    Put Privacy in the site style

---
+++ html/privacy.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "./wrapper-static.php";
+?>
+<?= startHead() ?>
+    <title>Starcraft 2 Co-op - Privacy policy</title>
+    <link rel="canonical" href="https://starcraft2coop.com/privacy">
+<?= startContent() ?>
+    <h1>Privacy Policy</h1>
+    <p>We don't do anything that requires a privacy policy. No ads, no accounts.</p>
+<?= endContent() ?>
